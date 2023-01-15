0e2d05169db7ab9c2293cfdf6079035edd33fbc0
aliJavaTar <ali77erfagh99@gmail.com>
2023-01-15 16:10:59 +0330

    add common-domain and add AggregateRoot

---
+++ common/common-domain/src/main/java/com/food/ordering/system/domain/AggregateRoot.java
@@ -0,0 +1,6 @@
+package com.food.ordering.system.domain;
+
+import com.food.ordering.system.domain.entity.base.BaseEntity;
+
+public abstract class AggregateRoot<ID> extends BaseEntity<ID> {
+}
--- common/common-domain/src/main/java/com/food/ordering/system/domain/entity/base/BaseEntity.java
+++ common/common-domain/src/main/java/com/food/ordering/system/domain/entity/base/BaseEntity.java
@@ -1,4 +1,4 @@
-package com.food.ordering.system.domain.entity;
+package com.food.ordering.system.domain.entity.base;
 
 import java.util.Objects;
 
